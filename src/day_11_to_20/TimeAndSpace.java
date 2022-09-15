5969f37d9be2dc04f070bf28eaaafd13e27d0886
alagu-p <alagappan.plr@gmail.com>
2022-09-15 09:35:51 +0530

    100 days coding challenge

---
+++ src/day_11_to_20/TimeAndSpace.java
@@ -0,0 +1,19 @@
+package day_11_to_20;
+
+/*
+ * What is Algorithm:
+ * 	A set of rules to be followed in calculations or other problem-solving operations
+ * or A procedure for solving a mathematical problem in a finite number of steps that frequently by recursive Operations
+ * 
+ * 	input ==>Algorithm==> Output 
+ */
+
+
+public class TimeAndSpace {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
