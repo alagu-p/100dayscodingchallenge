c62d5c2c9226a028bf19805cc03a29c1aa50f747
alagu-p <alagappan.plr@gmail.com>
2022-09-16 08:44:35 +0530

    Array All Operation Successfully learnt

--- src/day_11_to_20/Day_14.java
+++ src/day_11_to_20/Day_14.java
@@ -1,5 +1,7 @@
 package day_11_to_20;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class Day_14 {
 	/*
 	 * Algorithm is a step-but-step procedure,which defines a set of instructions to be executed in a certain oder to get the desired output
@@ -56,11 +58,69 @@ public class Day_14 {
 		//Basic Operations Array
 		// 1.Traverse
 		traverseOperation(arr);
+		System.out.println("-------------------------------------");
+		//2.Insertion
 		insertionOperation(arr);
+		System.out.println("-------------------------------------");
+		//3.Deletion Operation
+		deletionOperation(arr);
+		System.out.println("-------------------------------------");
+		//4.Search Operation
+		searchOperation(arr);
+		System.out.println("-------------------------------------");
+		//5.Update Operation
+		updateOperation(arr);
+			
+	}
+	
+	
+	private static void updateOperation(int[] arr) {
+	
+		int value=9,postion=4;
+		System.out.println("Update Array Position :"+postion+" value :"+value);
+		for(int i=0;i<arr.length;i++)
+		{
+			if(i==postion)
+			{
+				arr[i]=value;
+			}
+			System.out.println("Array Position :"+i+"  value :"+arr[i]);
+			
+		}
+		
+	}
+	private static void searchOperation(int[] arr) {
+		
+		int n=5;
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i]==n)
+			{
+				System.out.println("search value: "+n+"  Position :"+i);
+				break;
+			}
+		}
 		
 		
-
+	}
+	private static void deletionOperation(int[] arr) {
 		
+		int index=4;
+		int n=arr.length;
+		
+		int[] newarr=new int[n-1];
+		 
+		for(int i=0,k=0;i<n;i++)
+		{
+			if(i==index)
+				continue;
+			newarr[k++]=arr[i];
+		}
+		System.out.println("Deleting Index Postion :"+index);
+		System.out.println("After Deleting Array ");
+		
+		for(int i=0;i<newarr.length;i++)
+			System.out.println("Array "+i+"value :"+newarr[i]);
 		
 		
 	}
